8f2144b2dcab120ef0d4113df0460801b1075173
hasibulhasanshanto <kajal.shanto@gmail.com>
2019-12-19 22:20:59 +0600

    Uploading All files

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+       DB::table('users')->insert([
+           'role_id' => '1',
+           'name' => 'Mr. Admin',
+           'username' => 'admin',
+           'email' => 'admin@admin.com',
+           'password' => bcrypt('12345678'), 
+           
+        ]);
+
+            DB::table('users')->insert([
+           'role_id' => '2',
+           'name' => 'Mr. Author',
+           'username' => 'author',
+           'email' => 'author@author.com',
+           'password' => bcrypt('12345678'), 
+           
+        ]);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['as'=>'admin.','prefix'=>'admin','namespace'=>'Admin','middleware'=>['auth','admin']], function(){
+    Route::get('dashboard','DashboardController@index')->name('dashboard');
+});
+
+Route::group(['as'=>'author.','prefix'=>'author','namespace'=>'Author','middleware'=>['auth','author']], function(){
+    Route::get('dashboard','DashboardController@index')->name('dashboard');
+});
